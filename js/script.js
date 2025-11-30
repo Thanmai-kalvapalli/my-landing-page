907056c2d45577b3a89dd543f6cc6c8dfddf0f44
Thanmai-kalvapalli <thanmaikalvapalli@gmail.com>
2025-11-30 18:48:56 +0530

    Move script.js to correct js folder

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Mobile menu toggle
+const menuBtn = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('.nav-menu');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('open');
+    navMenu.classList.toggle('show');
+});
